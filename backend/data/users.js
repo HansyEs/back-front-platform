aa29f547a4c316af2d08767203bbf153aaf1cbf9
Anselmo Martín <anselmo.martin@talenttools.es>
2018-01-08 10:07:54 +0100

    Datos Fake de usuarios

---
+++ backend/data/users.js
@@ -0,0 +1,28 @@
+const users = [
+  {
+    id: 1,
+    nick_name: 'Hansy',
+    name: 'Anselmo',
+    surname: 'Martín Aller',
+    birth_date: '1967-02-17',
+    admin:true
+  },
+  {
+    id: 2,
+    nick_name: 'Javito',
+    name: 'Javier',
+    surname: 'Sigüenza el Sinvergüenza',
+    birth_date: '1993-06-21',
+    admin:true
+  },
+  {
+    id: 3,
+    nick_name: 'David',
+    name: 'David',
+    surname: 'El Gnomo',
+    birth_date: '1820-01-27',
+    admin:false
+  }
+];
+
+module.exports = users;
