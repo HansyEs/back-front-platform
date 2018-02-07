460dd44d8aff2839a1097fa73902f0871252166a
antonioMateos <antonio@freshcore.es>
2018-02-07 12:49:22 +0100

    #15 details

--- frontend/src/store/modules/getAuthUser_firebase.js
+++ frontend/src/store/modules/getAuthUser_firebase.js
@@ -32,7 +32,7 @@ const actions = {
 getAuthUser({state,commit,rootState}, user) {
 
   // GET AUTH USER INFO
-  console.log("Getting auth user.uid =",user.uid);
+  //console.log("Getting auth user.uid =",user.uid);
 
   var authUserID = 'http://localhost:3000/api/v1.0/authFirebase/'+user.uid;
 
@@ -41,7 +41,7 @@ getAuthUser({state,commit,rootState}, user) {
       if(response.status === 204){
         console.log("USER NOT AUTH"); // APP SHOULD NEVER GET HERE!
       }else if(response.status === 200){
-        console.log("AUTH UID :",response.data);
+        //console.log("AUTH UID :",response.data);
         if(response.data!=false){
           fetchUserData(response);
         }else{
@@ -55,7 +55,6 @@ getAuthUser({state,commit,rootState}, user) {
 
     function fetchUserData(obj){
       
-      //console.log(obj);
       var userUID = obj.data;
 
       // URL for AXIOS API request to Express Server
@@ -67,7 +66,7 @@ getAuthUser({state,commit,rootState}, user) {
             console.log("USER NOT FOUND"); // APP SHOULD NEVER GET HERE!
           }else if(response.status === 200){
             //state.userInfo = response.data;
-            console.log("USER RESP",response);
+            //console.log("USER RESP",response);
             commit(GET_AUTH_USER,response);
           }
         })
